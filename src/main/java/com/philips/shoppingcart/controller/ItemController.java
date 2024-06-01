d0a257c8dd7fa09a47aa4671423227df44de9caf
prinjal <prinjal.dave@gmail.com>
2024-06-01 07:13:46 -0700

    added rest end points for all the controllers

--- src/main/java/com/philips/shoppingcart/controller/ItemController.java
+++ src/main/java/com/philips/shoppingcart/controller/ItemController.java
@@ -1,4 +1,24 @@
 package com.philips.shoppingcart.controller;
 
+import com.philips.shoppingcart.model.Item;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/items")
 public class ItemController extends AbstractRestController{
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        return  null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAllItems() {
+        return  null;
+    }
 }
--- src/main/java/com/philips/shoppingcart/controller/ShoppingCartController.java
+++ src/main/java/com/philips/shoppingcart/controller/ShoppingCartController.java
@@ -1,7 +1,30 @@
 package com.philips.shoppingcart.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.philips.shoppingcart.model.Item;
+import com.philips.shoppingcart.model.ShoppingCart;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/shopping-carts")
 public class ShoppingCartController extends AbstractRestController{
+    @GetMapping("/{id}")
+    public ResponseEntity<ShoppingCart> getShoppingCartById(@PathVariable Long id) {
+        return  null;
+    }
+
+    @PostMapping("/{id}/items")
+    public ResponseEntity<Item> addItemToCart(@PathVariable Long id, @RequestBody Item item) {
+        return  null;
+    }
+
+    @PutMapping("/{id}/items/{itemId}")
+    public ResponseEntity<Item> updateItemInCart(@PathVariable Long id, @PathVariable Long itemId, @RequestBody Item item) {
+        return  null;
+    }
+
+    @DeleteMapping("/{id}/items/{itemId}")
+    public ResponseEntity<Void> removeItemFromCart(@PathVariable Long id, @PathVariable Long itemId) {
+        return  null;
+    }
 }
