c2f5e869630f3de692219e06593073f2f93a46b6
prinjal <prinjal.dave@gmail.com>
2024-06-01 05:08:44 -0700

    Added Product Entity

--- src/main/java/com/philips/shoppingcart/model/Product.java
+++ src/main/java/com/philips/shoppingcart/model/Product.java
@@ -1,4 +1,27 @@
 package com.philips.shoppingcart.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Product {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "product_id_seq"
+    )
+    private Long id;
+    @Column(
+            nullable = false
+    )
+    private String name;
+    @Column(
+            nullable = false
+    )
+    private double price;
 }
