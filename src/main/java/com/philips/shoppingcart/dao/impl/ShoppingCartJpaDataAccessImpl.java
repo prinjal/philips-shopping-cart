06e4138b286fb80b7144f96e00fe1de12628f856
prinjal <prinjal.dave@gmail.com>
2024-05-31 22:19:27 -0700

    Initialized project structre

---
+++ src/main/java/com/philips/shoppingcart/dao/impl/ShoppingCartJpaDataAccessImpl.java
@@ -0,0 +1,8 @@
+package com.philips.shoppingcart.dao.impl;
+
+import com.philips.shoppingcart.dao.ShoppingCartDAO;
+import org.springframework.stereotype.Component;
+
+@Component("ShoppingCartJpaDao")
+public class ShoppingCartJpaDataAccessImpl implements ShoppingCartDAO {
+}
