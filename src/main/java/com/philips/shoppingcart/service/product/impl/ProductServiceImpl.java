a63d5245a9cf2830eb64df3efa83d4089afb25a6
prinjal <prinjal.dave@gmail.com>
2024-06-02 11:34:05 -0700

    added service implementation for product

--- src/main/java/com/philips/shoppingcart/service/product/impl/ProductServiceImpl.java
+++ src/main/java/com/philips/shoppingcart/service/product/impl/ProductServiceImpl.java
@@ -1,40 +1,47 @@
 package com.philips.shoppingcart.service.product.impl;
 
+import com.philips.shoppingcart.dao.product.ProductDao;
 import com.philips.shoppingcart.model.Product;
 import com.philips.shoppingcart.service.product.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductDao productDao;
+
     @Override
     public List<Product> getAllProducts() {
-        return null;
+        return productDao.getAllProducts();
     }
 
     @Override
-    public Product getProductById(Long id) {
-        return null;
+    public Optional<Product> getProductById(Long id) {
+        return productDao.getProductById(id);
     }
 
     @Override
     public Product createProduct(Product product) {
-        return null;
+        return productDao.createProduct(product);
     }
 
     @Override
     public Product updateProduct(Long id, Product product) {
-        return null;
+        return productDao.updateProduct(id,product);
     }
 
     @Override
     public void deleteProduct(Long id) {
-
+        productDao.deleteProduct(id);
     }
 
     @Override
     public boolean productExists(String name) {
-        return false;
+        return productDao.productExists(name);
     }
 }
